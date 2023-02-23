bd5e81d4f28d75022ac286598c9caa863f3217a7
josem <josem@DESKTOP-Q9T0N5L>
2023-02-23 19:49:12 -0300

    Agregar archivos de proyecto.

---
+++ Inmobiliaria/Models/EmpleadoViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Inmobiliaria.Models
+{
+    public class EmpleadoViewModel
+    {
+        public int ID { get; set; }
+        public int idPerfil { get; set; }
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+        public string? NroDocumento { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public int idJornada { get; set; }
+        public bool Estado { get; set; }
+    }
+}
